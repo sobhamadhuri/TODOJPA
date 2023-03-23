d3963ae756e7c2513fe1ad592fddc43a95529530
sobhamadhuri <majjisobhamadhuri@gmail.com>
2023-03-23 08:05:06 +0000

    solution

---
+++ src/main/java/com/example/todo/repository/TodoJpaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * You can use the following import statements
+ * 
+ * import org.springframework.data.jpa.repository.JpaRepository;
+ * import org.springframework.stereotype.Repository;
+ * 
+ */
+
+// Write your code here
+package com.example.todo.repository;
+
+import com.example.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface TodoJpaRepository extends JpaRepository<Todo, Integer>{
+
+}
